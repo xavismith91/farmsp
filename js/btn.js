10bb77ca0e36898265887dc61e6c7e1f770bb119
Xavier Solano <xaviersolano91@gmail.com>
2024-07-03 17:48:01 -0600

    sweetalert

---
+++ js/btn.js
@@ -0,0 +1,10 @@
+$('#btn_inicioses ').click(function(){
+    Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Inicio de sesión correcta, Bienvenido",
+        showConfirmButton: false,
+        timer: 1500
+      });
+
+});
