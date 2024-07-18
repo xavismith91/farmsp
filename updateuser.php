29c31c7a4f59a1cd44b87ebe3853064450a574fd
xavismith91 <xaviersolano91@gmail.com>
2024-07-18 06:26:20 -0600

    nc2

--- updateuser.php
+++ updateuser.php
@@ -1,29 +1,6 @@
 <?php
 
     //incluimos las variables de conexión
-
-    include "conexion.php";
-
-
-    //delcalaracion de variables
-
-    $id = $_POST["id"];
-    $nombrec = $_POST["nombrec"];
-    $apellidos = $_POST["apellidos"];
-    $usuario = $_POST["usuario"];
-    $contrasena = $_POST["contrasena"]; 
-    $telefono = $_POST["telefono"];
-    $direccion = $_POST["direccion"];
-    $ciudad = $_POST["ciudad"];
-    $municipio = $_POST["municipio"];
-    $cp = $_POST["cp"];
-
-
-    
-
-
-
-<<<<<<< HEAD
         //declaracion de variables 
         $id=$_POST['id'];      
         $nombrec=$_POST["nombrec"];
@@ -73,10 +50,4 @@ else
        </script>';
 }
 mysqli_close($conn);
-  
- 
-=======
-
-
->>>>>>> a1bd693b59de19fed294fa4d0e79fde581eb31d2
  ?>
